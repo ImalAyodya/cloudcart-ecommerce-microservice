0f747c91dcbfaf6465a6c72a364631374bad9ac8
chamithusithmaka <177305347+chamithusithmaka@users.noreply.github.com>
2026-02-25 14:00:36 +0530

    Test azure CI CD pipeline

--- backend/productService/src/routes/healthRoutes.js
+++ backend/productService/src/routes/healthRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getHealth } = require("../controllers/healthController");
+const { getHealth, getInfo} = require("../controllers/healthController");
 
 // GET /api/health
 router.get("/", getHealth);
+
+// GET /api/health/info
+router.get("/info", getInfo);
 
 module.exports = router;
